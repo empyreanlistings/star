df9f821eac8b2547688b92e98d0a0da7a262ff55
Steven Bradford <stevenbradford@Stevens-MacBook-Pro.local>
2026-01-12 06:09:00 +0800

    feat(auth): fetch user avatar from Firestore 'Users' collection

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyArViUzMduVitt8FJDrSVPC_IQTeQrDFX4",
